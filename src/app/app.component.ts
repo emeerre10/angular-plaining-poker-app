bc4c03c4ac6df763e10ecf3c69892f3fb475ad1b
emeerre10 <mrmedina.is.m@gmail.com>
2023-08-28 18:14:50 +0200

    Add participants and select a card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Participant {
+  name: string;
+  selectedCard: number | undefined;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-planing-poker-app';
+
+  participants: Participant[] = [];
+
+  cards: number[] = [0, 1, 2, 3, 5, 8, 13, 21];
+
+  addParticipant(name: string): void {
+    this.participants.push({ name, selectedCard: undefined });
+  }
+
+  selectCardForParticipant(participant: Participant, selectCard: number): void {
+    this.participants[this.participants.indexOf(participant)].selectedCard = selectCard;
+  }
 }
